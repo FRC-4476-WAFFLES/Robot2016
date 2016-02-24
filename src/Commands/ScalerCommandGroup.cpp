3d37061450cc7e51cfcc2810bde937ed933ebce4
Duncan Stevenson <duncanstevenson@leahurstcollege.ca>
2016-02-23 21:44:38 -0500

    ADD RELAY TO COMMAND GROUPS
    PINK BUTTERFLIES

--- src/Commands/ScalerCommandGroup.cpp
+++ src/Commands/ScalerCommandGroup.cpp
@@ -4,14 +4,17 @@
 #include "ScalerScaleLong.h"
 #include "IntakeOut.h"
 //#include "WaitTrigger.h"
-
+#include "ScalerStop.h"
+#include "ScalerRelease.h"
 
 ScalerCommandGroup::ScalerCommandGroup() : CommandGroup()
 {
+	AddSequential(new ScalerRelease());
 	AddSequential(new IntakeOut());
 	AddSequential(new ScalerDeploy());
 	AddParallel(new IntakeOut());
 	AddSequential(new ScalerScaleLong(-3498*1.05));
 //	AddSequential(new WaitTrigger(8));
 	AddSequential(new ScalerScaleLong(395));
+	AddSequential(new ScalerStop());
 }
