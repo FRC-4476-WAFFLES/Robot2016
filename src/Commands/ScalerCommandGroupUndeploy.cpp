968eb42cd2aa983c9a736b694d2fac72d6aabd78
Ben-Alderson <bennyalderson@gmail.com>
2016-02-26 19:59:34 -0500

    Revert "ADD RELAY TO COMMAND GROUPS"
    This reverts commit 3d37061450cc7e51cfcc2810bde937ed933ebce4.

--- src/Commands/ScalerCommandGroupUndeploy.cpp
+++ src/Commands/ScalerCommandGroupUndeploy.cpp
@@ -4,13 +4,10 @@
 #include "ScalerScaleLong.h"
 #include "IntakeOut.h"
 //#include "WaitTrigger.h"
-#include "ScalerStop.h"
-#include "ScalerRelease.h"
 
 
 ScalerCommandGroupUndeploy::ScalerCommandGroupUndeploy() : CommandGroup()
 {
-	AddSequential(new ScalerRelease());
 	AddSequential(new IntakeOut());
 	AddSequential(new ScalerScaleLong(0));
 	AddParallel(new IntakeOut());
