4f268b1eb7379086ae83edd0ce78be4e43f11b9c
Ben Alderson <bennyalderson@gmail.com>
2016-02-29 19:17:45 -0500

    Add empty DriverInfo.h

---
+++ src/DriverInfo.h
@@ -0,0 +1,2 @@
+// todo: Add information like button mappings and general info for drivers here.
+// todo: Put this file on the desktop of the driver station for later reference and tell the drivers about it
